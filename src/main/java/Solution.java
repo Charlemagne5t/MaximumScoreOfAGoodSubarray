5c6e946b4edb47345d558d8499f2a1b8e436cf2d
alek <wait82985@gmail.com>
2023-10-22 15:00:35 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,37 @@
 public class Solution {
     public int maximumScore(int[] nums, int k) {
-        return 0;
+        int left = k;
+        int right = k;
+        int maxTotalSum = nums[k];
+        int currentMin = nums[k];
+
+        while (left >= 0 || right < nums.length){
+            if(left > 0 && right < nums.length - 1){
+                if(nums[left - 1] >= currentMin && nums[right + 1] >= currentMin){
+                    left--;
+                    right++;
+                    maxTotalSum = Math.max(maxTotalSum, currentMin * (right - left + 1));
+                } else if (nums[left - 1] >= nums[right + 1]) {
+                    left--;
+                    currentMin = Math.min(currentMin, nums[left]);
+                    maxTotalSum = Math.max(maxTotalSum, currentMin * (right - left + 1));
+                }else {
+                    right++;
+                    currentMin = Math.min(currentMin, nums[right]);
+                    maxTotalSum = Math.max(maxTotalSum, currentMin * (right - left + 1));
+                }
+            }else if(left == 0 && right < nums.length - 1){
+                right++;
+                currentMin = Math.min(currentMin, nums[right]);
+                maxTotalSum = Math.max(maxTotalSum, currentMin * (right - left + 1));
+            } else if (left > 0 && right == nums.length - 1) {
+                left--;
+                currentMin = Math.min(currentMin, nums[left]);
+                maxTotalSum = Math.max(maxTotalSum, currentMin * (right - left + 1));
+            } else if (left == 0 && right == nums.length - 1) {
+                break;
+            }
+        }
+        return maxTotalSum;
     }
 }
